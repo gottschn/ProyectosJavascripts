dad9b3d65b4896d30d5906ce3b9a39facae1d516
Gott <stbn.schn@gmail.com>
2022-03-31 01:44:48 -0300

    terminamos proyecto-17

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -30,7 +30,7 @@ const min = max - 10;
 //Eventos
 
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos(); // Muestra los autos al cargar
+    mostrarAutos(autos); // Muestra los autos al cargar
     
     llenarSelect(); //llena los años
 })
@@ -39,36 +39,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
 marca.addEventListener('change', e =>{
     datosBusqueda.marca = e.target.value;
+
+    filtrarAuto();
 });
 
 year.addEventListener('change',  e =>{
-    datosBusqueda.year = e.target.value;
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 minimo.addEventListener('change', e =>{
-    datosBusqueda.minimo = e.target.value;
+    datosBusqueda.minimo = parseInt(e.target.value);
+
+    filtrarAuto();
 })
 
 maximo.addEventListener('change', e =>{
-    datosBusqueda.maximo = e.target.value;
+    datosBusqueda.maximo = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 puertas.addEventListener('change', e => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 transmision.addEventListener('change', e =>{
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 })
 
 color.addEventListener('change', e =>{
     datosBusqueda.color = e.target.value;
     console.log(datosBusqueda);
+    filtrarAuto();
 })
 
 //Funciones
 
-function mostrarAutos(){
+function mostrarAutos(autos){
+
+    limpiarHTML();
     autos.forEach(auto => {
         const {marca,modelo,year,precio,puertas,color,transmision} = auto;
         const autoHTML = document.createElement('p');
@@ -84,9 +95,17 @@ function mostrarAutos(){
         `
 
         //insertar el HTML
-
         resultado.appendChild(autoHTML);
+
     });
+}
+
+//Limpiar HTML
+
+function limpiarHTML (){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 }
 
 function llenarSelect() {
@@ -96,4 +115,86 @@ function llenarSelect() {
         opcion.textContent = i;
         year.appendChild(opcion); // Agrega las opciones de año
     }
+}
+
+//Funcion que filtra en base a la busqueda
+function filtrarAuto(){
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarPrecioMin).filter(filtrarPrecioMax).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor)
+
+
+   if(resultado.length){
+    mostrarAutos(resultado);
+   }
+   else{
+       noResultado();
+   }
+}
+
+function noResultado(){
+    limpiarHTML();
+
+     const noResultado = document.createElement('div');
+     noResultado.classList.add('alerta','error');
+     noResultado.textContent = ('No hubo resultados, intenta con otros terminos de busqueda.');
+     resultado.appendChild(noResultado);
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if (marca){
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear (auto){
+    const {year} = datosBusqueda;
+    if (year){
+        return auto.year === year;
+    }
+    return auto;
+}
+
+ function filtrarPrecioMin(auto){
+
+    const {minimo} = datosBusqueda;
+    if(minimo){
+        return auto.precio >= minimo;
+    }
+    return auto;
+} 
+
+function filtrarPrecioMax(auto){
+    const {maximo} = datosBusqueda;
+    if(maximo){
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+
+function filtrarPuertas(auto){
+    const {puertas} = datosBusqueda;
+
+    if(puertas){
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto){
+    const {transmision} = datosBusqueda;
+
+    if(transmision){
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto){
+    const {color} = datosBusqueda;
+
+    if(color){
+        return auto.color === color;
+    }
+    return auto;
 }
